20d655c31232b284ec41777a0f001d4259694c02
Bartosz Piechowiak <piechowiaq@gmail.com>
2023-04-03 21:17:16 +0200

    Adding comments

--- Validator.php
+++ Validator.php
@@ -16,11 +16,22 @@ class Validator
 
     public function validate(): array
     {
+        /*foreach array with rules as name firstname, email, password => rules required, min:3*/
         foreach($this->rules as $name => $rules){
+            /*foreach array with rules required, min:3*/
             foreach ($rules as $rule){
+                /*if : found explode for min & 3*/
                 $rule_value = explode(':', $rule);
+                /*rule_name = min as $rule_value[0] - first element of explode*/
                 $rule_name =  $rule_value[0];
+                /*if key 1 exist in array value = key[1] which is 3*/
                 $value = (array_key_exists(1, $rule_value)) ? $rule_value[1] : null;
+
+                /*name => first name , $value is null if required if min it is 3*/
+                echo "<pre>";
+                var_dump($this->$rule_name($name, $value));
+                echo "</pre>";
+                exit;
                 if (!$this->$rule_name($name, $value)) break 2;
             }
         }
